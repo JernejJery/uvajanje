73d88e978832b4c7b6b835318925f59f1b1fb771
JernejJery <jernej.orovic.00@gmail.com>
2017-07-18 13:03:57 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web;
-//using System.Web.Extensions;
 using System.Web.Script.Serialization;
+using System.IO;
+
 
 namespace uvajanje_JSON
 {
@@ -20,14 +21,35 @@ namespace uvajanje_JSON
         public Date dateOfBirth;
     }
 
+
+
     class Program
     {
+
+        static object toFile(object obj) //objekt -> json
+        {
+            var json = new JavaScriptSerializer().Serialize(obj);
+
+            System.IO.File.WriteAllText("out.txt", json.ToString());
+            return json;
+        }
+
+        static object toObject(string json) //json -> objekt
+        {
+            JavaScriptSerializer oJS = new JavaScriptSerializer();
+            object oRootObject = new object();
+            oRootObject = oJS.Deserialize<object>(json);
+
+            return oRootObject;
+        }
+
+
         static void Main()
         {
             var obj = new Person
             {
-                firstName = "Markoff",
-                lastName = "Chaney",
+                firstName = "Jernej",
+                lastName = "Orovič",
                 dateOfBirth = new Date
                 {
                     y = 1996,
@@ -35,8 +57,18 @@ namespace uvajanje_JSON
                     d = 8
                 }
             };
-            var json = new JavaScriptSerializer().Serialize(obj);
-            Console.WriteLine(json);
+            try
+            {
+                string s = System.IO.File.ReadAllText("oudt.txt");
+                toObject(s);
+            }
+            catch (Exception e) when (e.Message.Contains("Could not find file"))
+            {
+                Console.WriteLine("No file found");
+            }
+            
+
+            Console.WriteLine(toFile(obj).ToString());
 
             Console.ReadKey();
         }
